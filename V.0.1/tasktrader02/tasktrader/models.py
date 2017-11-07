38b87fd886791aac58af6e5692d1987c7f29c50b
ayser259 <ayserchoudhury@gmail.com>
2017-11-06 23:15:27 -0500

    Refer to changes as of Development Log 23-14-6th-nov-17

--- V.0.1/tasktrader02/tasktrader/models.py
+++ V.0.1/tasktrader02/tasktrader/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+# Models representing objects that will make up tasktrader
+
+class Company(models.Model):
+    id = models.AutoField(primary_key =True)
+    company_name = models.CharField(max_length = 30)
+
+    def __str__(self): # returns the string value for the company name and company ID
+        return_value = (self.id + " , " +str(self.company_name))
+        return return_value
+
+class Location(models.Model):
+    id = models.AutoField(primary_key=True)
+    campus_name = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    street_address = models.CharField(max_length=30)
+    postal_code = models.CharField(max_length=30)
+
+    def __str__(self): # returns the string value for the Location
+        return_value = (self.id + " , " +str(self.campus_name)+ " , " +str(self.city)+ " , " +str(self.country)+ " , " +str(self.street_address)+ " , " +str(self.postal_code))
+        return return_value
+
+class Department(models.Model):
+    id = models.AutoField(primary_key = True)
+    company_id = models.ForeignKey(Company, on_delete=models.CASCADE)
+    department_name = models.CharField(max_length=30)
+
+    def __str__(self): # returns the string value for the Location
+        return_value = (self.id + " , " +str(self.company_id.company_name)+ " , " +str(self.department_name))
+        return return_value
--- V.0.1/tasktrader02/tasktrader/views.py
+++ V.0.1/tasktrader02/tasktrader/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Views to access webpages created
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
